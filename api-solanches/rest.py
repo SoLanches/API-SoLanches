e04ed91bb3d2a4ac1996afd02b1b7e8f01a9dde1
claudiodantas <franciclaudio.silva@ccc.ufcg.edu.br>
2021-07-13 19:41:01 -0300

    implementa a rota status

---
+++ api-solanches/rest.py
@@ -0,0 +1,19 @@
+import time
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+started_at = time.time()
+
+
+@app.route("/status", methods=["GET"])
+def status():
+    status = {
+        "status": "operacional",
+        "service": "api-solanches",
+        "started_at": started_at,
+        "timestamp": time.time()
+    }
+    return status, 200
