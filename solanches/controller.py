54b869071c520ac5e69d5e85533023c8c25f6c7c
GusttaFS <gustavo.silva@ccc.ufcg.edu.br>
2021-07-19 16:49:21 -0300

    Modifica tipo de categoria.

--- solanches/controller.py
+++ solanches/controller.py
@@ -1,11 +1,12 @@
 from . models import Produto
 
+
 def cadastra_produto(titulo, descricao, imagem, preco, categoria):
     assert(titulo and type(titulo) is str, "Erro: titulo inválido!")
     assert(descricao and type(descricao) is str, "Erro: descricao inválida!")
     #assert(imagem and type(imagem) is str, "Erro: imagem inválida!")
     assert(preco and type(preco) is float, "Erro: preco inválido!")
-    assert(categoria and type(categoria) is float, "Erro: categoria inválida!")
+    assert(categoria and type(categoria) is str, "Erro: categoria inválida!")
 
     novo_produto = Produto(titulo, descricao, preco, categoria)
     produto_id = novo_produto.save()
