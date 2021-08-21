403d32a1866bb6cafc4f1eb49773e8126c9d7149
claudiodantas <franciclaudio.silva@ccc.ufcg.edu.br>
2021-08-21 11:39:07 -0300

    adiciona teste para a rota /status

---
+++ solanches/tests/test_rest.py
@@ -0,0 +1,21 @@
+import pytest
+
+
+@pytest.fixture
+def client(rest):
+    client = rest.app.test_client()
+    return client
+
+
+def test_status(client):
+    expected_keys = ["status", "timestamp", "started_at", "service"]
+    response = client.get('/status')
+    status = response.json
+    assert response.status_code == 200
+    assert all(key in status for key in expected_keys)
+    assert type(status['status']) is str
+    assert type(status['timestamp']) is float
+    assert type(status['started_at']) is float
+    assert type(status['service']) is str
+    assert status['status'] == 'operacional'
+    assert status['service'] == 'api-solanches'
