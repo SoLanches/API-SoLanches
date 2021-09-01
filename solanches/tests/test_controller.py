d13c168372a178860e097ef274eb66b99675ae7f
Eduardo Nunes <50140955+EduardoNunes5@users.noreply.github.com>
2021-09-01 17:34:35 -0300

    Removendo código de outra branch

--- solanches/tests/test_controller.py
+++ solanches/tests/test_controller.py
@@ -1,19 +1,1 @@
-import pytest
 
-
-def test_remove_comercio_inexistente(controller):
-    with pytest.raises(Exception) as execinfo:
-        comercio_nome = 'comercio_sem_id'
-        controller.remove_comercio(comercio_nome)
-    assert str(execinfo.value) == f'Erro: comercio com nome {comercio_nome} não cadastrado!'
-
-
-def test_remove_comercio_sucesso(controller):
-    comercio_nome = 'comercio_test'
-    comercio_attributes = {
-            'telefone': '83999999999'
-    }
-
-    controller.cadastra_comercio(comercio_nome, comercio_attributes)
-    result = controller.remove_comercio(comercio_nome)
-    assert result == 1
