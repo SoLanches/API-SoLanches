628c7f042b711f55c5fe9373a8d3db5771a26f23
Eduardo Nunes <50140955+EduardoNunes5@users.noreply.github.com>
2021-09-01 17:42:56 -0300

    Consertando variáveis

--- solanches/__main__.py
+++ solanches/__main__.py
@@ -5,10 +5,11 @@ from . rest import app
 
 load_dotenv()
 
-SERVER_URL = f"{os.getenv('SOLANCHES_SERVER_HOST')}:{os.getenv('SOLANCHES_SERVER_PORT')}"
+SERVER_HOST = os.getenv('SOLANCHES_SERVER_HOST')
+SERVER_PORT = os.getenv('SOLANCHES_SERVER_PORT')
+SERVER_URL = f"{SERVER_HOST}:{SERVER_PORT}"
 
 url = urlparse(SERVER_URL)
 host, port = url.hostname, url.port
 print(f"Iniciando servidor em: {url.geturl()}")
-if __name__ == "__main__":
-    app.run(host=host, port=port, debug=True)
+app.run(host=host, port=port, debug=True)
