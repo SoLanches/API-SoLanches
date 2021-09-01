5ab980ff0a21e65c00b9b7effa90bb9d006d761e
Eduardo Nunes <50140955+EduardoNunes5@users.noreply.github.com>
2021-09-01 17:33:43 -0300

    removendo código de outra branch

--- solanches/tests/test_rest.py
+++ solanches/tests/test_rest.py
@@ -19,25 +19,3 @@ def test_status(client):
     assert type(status['service']) is str
     assert status['status'] == 'operacional'
     assert status['service'] == 'api-solanches'
-
-
-def test_remove_comercio_sucesso(client, controller):
-    comercio_nome = 'comercio_teste'
-    controller.cadastra_comercio(comercio_nome, {'telefone': '83999999999'})
-    url = f'/comercio/{comercio_nome}'
-    response = client.delete(url)
-    responseJson = response.json
-    assert response.status_code == 200
-    assert responseJson['message'] == f'comercio {comercio_nome} removido com sucesso'
-    assert responseJson['status_code'] == 200
-    
-
-def test_remove_comercio_inexistente(client):
-    comercio_nome = 'comercio_teste'
-    url = f'/comercio/{comercio_nome}'
-    response = client.delete(url)
-    responseJson = response.json
-    assert response.status_code == 400
-    assert responseJson['message'] == f'Erro: comercio com nome {comercio_nome} não cadastrado!'
-    assert responseJson['status_code'] == 400
-
