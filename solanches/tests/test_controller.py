f5db21ec5684bd509a53b60ef7641a8f77d6c408
eduardo <eduardo.afonso.silva@ccc.ufcg.edu.br>
2021-09-03 20:45:56 -0300

    remove comercio controller test

--- solanches/tests/test_controller.py
+++ solanches/tests/test_controller.py
@@ -1,5 +1,5 @@
 import pytest
-
+from unittest import mock
 
 def test_remove_comercio_inexistente(controller):
     with pytest.raises(Exception) as execinfo:
@@ -8,12 +8,17 @@ def test_remove_comercio_inexistente(controller):
     assert str(execinfo.value) == f'Erro: comercio com nome {comercio_nome} não cadastrado!'
 
 
-def test_remove_comercio_sucesso(controller):
+@mock.patch('solanches.models.Comercio.get_by_name')
+@mock.patch('solanches.models.Comercio.remove_comercio')
+@mock.patch('solanches.models.Cardapio.get_by_id')
+def test_remove_comercio_sucesso(mock_get_by_id, mock_remove_comercio, mock_get_by_name, controller):
     comercio_nome = 'comercio_test'
     comercio_attributes = {
             'telefone': '83999999999'
     }
-
-    controller.cadastra_comercio(comercio_nome, comercio_attributes)
+    mock_get_by_name.return_value = {'nome': comercio_nome,
+                                           'attributes': comercio_attributes}
+    mock_get_by_id.return_value = {'produtos': []}
+    mock_remove_comercio.return_value = 1
     result = controller.remove_comercio(comercio_nome)
     assert result == 1
