3bd08d19a0311c4d6fc4048150ec00866de379b9
claudiodantas <franciclaudio.silva@ccc.ufcg.edu.br>
2021-09-03 12:23:34 -0300

    fix: corrige import do pymongo
    a forma que estava impedia o controller de acessar o connect2db.pymongo

--- solanches/connect2db.py
+++ solanches/connect2db.py
@@ -1,4 +1,4 @@
-from pymongo import MongoClient
+import pymongo
 import os
 from dotenv import load_dotenv
 
@@ -7,6 +7,6 @@ load_dotenv()
 DB_HOST = os.getenv("SOLANCHES_DB_HOST")
 DB_PORT = int(os.getenv("SOLANCHES_DB_PORT"))
 
-MONGO_CLIENT = MongoClient(DB_HOST, DB_PORT)
+MONGO_CLIENT = pymongo.MongoClient(DB_HOST, DB_PORT)
 
 DB = MONGO_CLIENT.SolanchesDB
