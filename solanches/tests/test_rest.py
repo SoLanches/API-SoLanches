7920d5ea72256ea5826ccce56dd203289ef4b05a
eduardo <eduardo.afonso.silva@ccc.ufcg.edu.br>
2021-09-03 21:13:07 -0300

    controller test remove comercio

--- solanches/tests/test_rest.py
+++ solanches/tests/test_rest.py
@@ -8,18 +8,18 @@ def client(rest):
     return client
 
 
-# def test_status(client):
-#     expected_keys = ["status", "timestamp", "started_at", "service"]
-#     response = client.get('/status')
-#     status = response.json
-#     assert response.status_code == 200
-#     assert all(key in status for key in expected_keys)
-#     assert type(status['status']) is str
-#     assert type(status['timestamp']) is float
-#     assert type(status['started_at']) is float
-#     assert type(status['service']) is str
-#     assert status['status'] == 'operacional'
-#     assert status['service'] == 'api-solanches'
+def test_status(client):
+    expected_keys = ["status", "timestamp", "started_at", "service"]
+    response = client.get('/status')
+    status = response.json
+    assert response.status_code == 200
+    assert all(key in status for key in expected_keys)
+    assert type(status['status']) is str
+    assert type(status['timestamp']) is float
+    assert type(status['started_at']) is float
+    assert type(status['service']) is str
+    assert status['status'] == 'operacional'
+    assert status['service'] == 'api-solanches'
 
 
 @mock.patch('solanches.rest.controller.remove_comercio')
